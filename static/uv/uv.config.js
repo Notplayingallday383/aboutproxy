f6e6258b0168f131d95efcbba7e03cf80975dc01
John <72810050+Notplayingallday383@users.noreply.github.com>
2023-11-08 12:15:17 -0500

    Update Bares

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,7 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
     prefix: '/service/ultraviolet/',
-    bare: '/bare/',
+    bare: 'https://anura.mercurywork.shop/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
